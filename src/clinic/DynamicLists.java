f1d5e38e513a351ea84e941ebea95e5f9405eb4b
dylpb <dylanpabri@gmail.com>
2026-08-14 14:54:18 -0600

    Se crea la interfaz
    La interfasz generica se crea con los 4 metodos

---
+++ src/clinic/DynamicLists.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package clinic;
+
+/**
+ *
+ * @author Dylan
+ * La T (tipo) es una especie de parametro que nos cambia la interfaz a una interfaz generica
+ * (no se debe llamar estrictamente T)
+ * lo que le permite trabajar con cualquier tipo de datos, int, string. patient, etc.
+ */
+public interface DynamicLists <T> { 
+    
+    public boolean add(T item);
+    public iterator getAll();
+    public int size();
+    public boolean isEmpty();
+    
+}
