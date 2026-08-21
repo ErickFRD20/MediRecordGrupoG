cd34c6b085b55d1be489b04a4c921e32496044d1
Marco Ramirez Quiros <maaramirezqu@est.utn.ac.cr>
2026-08-21 14:48:47 -0600

    Se termino la clase AppointmentsList.java

--- src/appointments/AppointmentsList.java
+++ src/appointments/AppointmentsList.java
@@ -5,6 +5,7 @@
 package appointments;
 
 import clinic.KeyDynamicsLists;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 /**
@@ -18,5 +19,46 @@ public class AppointmentsList implements
     public AppointmentsList(){
         this.appointments = new TreeSet();
     }
-    
+
+    @Override
+    public Appointment get(String id) {
+        for(Appointment appointment : this.appointments){
+            if(appointment.getCode().equals(id)){
+                return appointment;
+            }
+        }
+        return null;    }
+
+    @Override
+    public boolean remove(String id) {
+        Appointment found = get(id);
+        if(found == null){
+            return false;
+        }
+        return this.appointments.remove(found);
+    }
+
+    @Override
+    public boolean add(Appointment item) {
+        return this.appointments.add(item);
+
+    }
+
+    @Override
+    public Iterator getAll() {
+        return this.appointments.iterator();
+
+    }
+
+    @Override
+    public int size() {
+        return this.appointments.size();
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.appointments.isEmpty();
+
+    }
 }
